959707e94a432f058fb3be8f3f6c0a4f20f736c4
Chin Sek Yi <sekyichin@gmail.com>
2024-12-19 01:59:29 +0800

    Complete data cleaning

--- code_notes.py
+++ code_notes.py
@@ -2,8 +2,8 @@ numerical_columns = df.select_dtypes(include=['number'])
 correlation_matrix = numerical_columns.corr()
 correlation_matrix
 
-import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 plt.figure(figsize=(12, 8))
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
